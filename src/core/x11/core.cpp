a5b672fce5bb0ebae61ff24ea5aac4fd901e205d
waneon <waneon.kim@gmail.com>
2022-02-28 00:14:23 +0900

    Improve x11 error handling

--- src/core/x11/core.cpp
+++ src/core/x11/core.cpp
@@ -10,17 +10,20 @@
 #include "translate.h"
 
 // register keyboard shortcut
-static bool register_key(const KeyShortcut &, std::string_view, Display *,
+static void register_key(const KeyShortcut &, std::string_view, Display *,
                          Window);
 // un-register keyboard shortcut
-static bool unregister_key(const KeyShortcut &, std::string_view, Display *,
+static void unregister_key(const KeyShortcut &, std::string_view, Display *,
                            Window);
 // register mouse shortcut
-static bool register_button(const ButtonShortcut &, std::string_view, Display *,
+static void register_button(const ButtonShortcut &, std::string_view, Display *,
                             Window);
 // un-register mouse shortcut
-static bool unregister_button(const ButtonShortcut &, std::string_view,
+static void unregister_button(const ButtonShortcut &, std::string_view,
                               Display *, Window);
+
+// x11 error handler
+static int x11_error_handler(Display *, XErrorEvent *e);
 
 std::string get_config_file() {
 #if defined(NDEBUG)
@@ -30,9 +33,12 @@ std::string get_config_file() {
 }
 
 bool run(const Config &config) {
-    static Display *dpy = XOpenDisplay(0);
-    static Window root = XDefaultRootWindow(dpy);
+    Display *dpy = XOpenDisplay(0);
+    Window root = XDefaultRootWindow(dpy);
     XEvent ev;
+
+    // set error handler
+    XSetErrorHandler(x11_error_handler);
 
     // Magnifier
     Magnifier *magnifier = nullptr;
@@ -46,24 +52,17 @@ bool run(const Config &config) {
     }
 
     // register shortcuts
-    if (!register_key(config.toggle_key, "toggle", dpy, root))
-        return false;
-    if (!register_key(config.shrink_key, "shrink", dpy, root))
-        return false;
-    if (!register_key(config.enlarge_key, "enlarge", dpy, root))
-        return false;
-    if (!register_key(config.exit_key, "exit", dpy, root))
-        return false;
+    register_key(config.toggle_key, "toggle", dpy, root);
+    register_key(config.shrink_key, "shrink", dpy, root);
+    register_key(config.enlarge_key, "enlarge", dpy, root);
+    register_key(config.exit_key, "exit", dpy, root);
 
-    if (!register_button(config.toggle_button, "toggle", dpy, root))
-        return false;
-    if (!register_button(config.shrink_button, "shrink", dpy, root))
-        return false;
-    if (!register_button(config.enlarge_button, "enlarge", dpy, root))
-        return false;
-    if (!register_button(config.exit_button, "exit", dpy, root))
-        return false;
+    register_button(config.toggle_button, "toggle", dpy, root);
+    register_button(config.shrink_button, "shrink", dpy, root);
+    register_button(config.enlarge_button, "enlarge", dpy, root);
+    register_button(config.exit_button, "exit", dpy, root);
 
+    // select events
     XSelectInput(dpy, root, KeyPressMask);
     XSelectInput(dpy, root, ButtonPressMask);
     while (true) {
@@ -99,80 +98,54 @@ out:
     XCloseDisplay(dpy);
 
     // un-register shortcuts
-    if (!unregister_key(config.toggle_key, "toggle", dpy, root))
-        return false;
-    if (!unregister_key(config.enlarge_key, "enlarge", dpy, root))
-        return false;
-    if (!unregister_key(config.shrink_key, "shrink", dpy, root))
-        return false;
-    if (!unregister_key(config.exit_key, "exit", dpy, root))
-        return false;
+    unregister_key(config.toggle_key, "toggle", dpy, root);
+    unregister_key(config.shrink_key, "shrink", dpy, root);
+    unregister_key(config.enlarge_key, "enlarge", dpy, root);
+    unregister_key(config.exit_key, "exit", dpy, root);
 
-    if (!unregister_button(config.toggle_button, "toggle", dpy, root))
-        return false;
-    if (!unregister_button(config.enlarge_button, "enlarge", dpy, root))
-        return false;
-    if (!unregister_button(config.shrink_button, "shrink", dpy, root))
-        return false;
-    if (!unregister_button(config.exit_button, "exit", dpy, root))
-        return false;
+    unregister_button(config.toggle_button, "toggle", dpy, root);
+    unregister_button(config.shrink_button, "shrink", dpy, root);
+    unregister_button(config.enlarge_button, "enlarge", dpy, root);
+    unregister_button(config.exit_button, "exit", dpy, root);
 
     return true;
 }
 
-bool register_key(const KeyShortcut &shortcut, std::string_view name,
+void register_key(const KeyShortcut &shortcut, std::string_view name,
                   Display *dpy, Window root) {
     if (shortcut.state != ShortcutState::NONE) {
-        if (!XGrabKey(dpy, shortcut.key, shortcut.modifiers, root, False,
-                      GrabModeAsync, GrabModeAsync)) {
-            spdlog::error("Could not register {} key shortcut.", name);
-            return false;
-        } else {
-            spdlog::info("Successfully registered {} key shortcut.", name);
-        };
+        XGrabKey(dpy, shortcut.key, shortcut.modifiers, root, False,
+                 GrabModeAsync, GrabModeAsync);
     }
-    return true;
 }
 
-static bool unregister_key(const KeyShortcut &shortcut, std::string_view name,
-                           Display *dpy, Window root) {
+void unregister_key(const KeyShortcut &shortcut, std::string_view name,
+                    Display *dpy, Window root) {
     if (shortcut.state != ShortcutState::NONE) {
-        if (!XUngrabKey(dpy, shortcut.key, shortcut.modifiers, root)) {
-            spdlog::error("Could not un-register {} key shortcut.", name);
-            return false;
-        } else {
-            spdlog::info("Successfully un-registered {} key shortcut.", name);
-        };
+        XUngrabKey(dpy, shortcut.key, shortcut.modifiers, root);
     }
-    return true;
 }
 
-bool register_button(const ButtonShortcut &shortcut, std::string_view name,
+void register_button(const ButtonShortcut &shortcut, std::string_view name,
                      Display *dpy, Window root) {
     if (shortcut.state != ShortcutState::NONE) {
-        if (!XGrabButton(dpy, shortcut.button, shortcut.modifiers, root, False,
-                         ButtonPressMask, GrabModeAsync, GrabModeAsync, None,
-                         None)) {
-            spdlog::error("Could not register {} button shortcut.", name);
-            return false;
-        } else {
-            spdlog::info("Successfully registered {} button shortcut.", name);
-        };
+        XGrabButton(dpy, shortcut.button, shortcut.modifiers, root, False,
+                    ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
     }
-    return true;
 }
 
-static bool unregister_button(const ButtonShortcut &shortcut,
-                              std::string_view name, Display *dpy,
-                              Window root) {
+void unregister_button(const ButtonShortcut &shortcut, std::string_view name,
+                       Display *dpy, Window root) {
     if (shortcut.state != ShortcutState::NONE) {
-        if (!XUngrabButton(dpy, shortcut.button, shortcut.modifiers, root)) {
-            spdlog::error("Could not un-register {} button shortcut.", name);
-            return false;
-        } else {
-            spdlog::info("Successfully un-registered {} button shortcut.",
-                         name);
-        };
+        XUngrabButton(dpy, shortcut.button, shortcut.modifiers, root);
     }
-    return true;
+}
+
+int x11_error_handler(Display *dpy, XErrorEvent *e) {
+    char error_msg[256];
+
+    XGetErrorText(dpy, e->error_code, error_msg, sizeof(error_msg));
+    spdlog::error("X11 error occured: {}, serial number: {}", error_msg, e->serial);
+
+    return 0;
 }
