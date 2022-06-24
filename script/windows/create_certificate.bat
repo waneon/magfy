7678d58ff87b5654fb6c18b49cd43759bfa89f4c
waneon <waneon.kim@gmail.com>
2022-06-24 21:52:29 +0900

    Update windows script file

--- script/windows/create_certificate.bat
+++ script/windows/create_certificate.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set CERT_PATH="Cert:\\CurrentUser\\My"
+set CERT_PATH="Cert:/CurrentUser/My"
+set /p OUTPUT_PATH=Input the output certificate file path: 
 
 powershell "if ( !(Get-ChildItem '%CERT_PATH%' | where { $_.subject -eq 'CN=magfy' }) ) { New-SelfSignedCertificate -Type CodeSigningCert -Subject 'CN=magfy' -CertStoreLocation '%CERT_PATH%' -NotAfter (Get-Date).AddYears(10) }"
-powershell "Export-Certificate -Cert ( Get-ChildItem '%CERT_PATH%' | where { $_.subject -eq 'CN=magfy' } ) -FilePath '../../build/magfy.crt'"
+powershell "Export-Certificate -Cert ( Get-ChildItem '%CERT_PATH%' | where { $_.subject -eq 'CN=magfy' } ) -FilePath '%OUTPUT_PATH%'"
--- script/windows/sign.bat
+++ script/windows/sign.bat
@@ -2,5 +2,5 @@
 
 set /p BINARY_PATH=Input the binary file path: 
 
-mt -manifest "../../manifest/magfy.manifest" -outputresource:"%BINARY_PATH%";#1
+mt -manifest "%~dp0/../../manifest/magfy.manifest" -outputresource:"%BINARY_PATH%";#1
 SignTool sign /v /n "magfy" /t http://timestamp.digicert.com "%BINARY_PATH%"
